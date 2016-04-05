9f61dfb816d3e07157deace990ba4547c53efec5
diedexx <diedex10@yahoo.com>
2016-04-05 22:46:07 +0200

    omg <>

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 module.exports = {
-
-    'url': 'mongodb://localhost:27017/restrace',
-    'testurl': 'mongodb://localhost:27017/restracetest',
-    'remoteDB': 'mongodb://<user>:<user>@ds013260.mlab.com:13260/webs5restrace'
+    url: 'mongodb://localhost:27017/restrace',
+    testurl: 'mongodb://localhost:27017/restracetest',
+    remoteDB: 'mongodb://user:user@ds013260.mlab.com:13260/webs5restrace'
 
 };
--- models/models.js
+++ models/models.js
@@ -2,7 +2,6 @@ var requireModels = function(){
     require('./user.js');
     require('./waypoint.js');
     require('./race.js');
-    require('./completion.js');
 };
 
 module.exports = requireModels;
